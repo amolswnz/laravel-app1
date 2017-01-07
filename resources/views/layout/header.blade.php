f9e50d65c4dca023f9b2f106d6d4c570d3a8080e
amolswnz <amolsw.nz@gmail.com>
2017-01-07 15:26:24 +1300

    Heading updated

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lumen - Blog</title>
+    <title>MyBlog using Laravel</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
     <style type="text/css">
